098fe01bcfb65041fdc4bd19657c68363b0184dd
vraiSlophil <slophil.contact@gmail.Com>
2023-05-22 11:20:05 +0200

    + cahier des charges pour cote python + correction websocket connexion

--- html_client_test/js/script.js
+++ html_client_test/js/script.js
@@ -5,6 +5,7 @@ const testButton = document.querySelector("#test_message_button");
 // Connection opened
 socket.addEventListener('open', function (event) {
     console.log('Connected to the WS Server!')
+    socket.send('TOKEN TEST');
 });
 
 // Connection closed
--- web_socket_server/server.py
+++ web_socket_server/server.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 import asyncio
+
 from websockets.server import serve
 
 
@@ -9,9 +10,30 @@ async def echo(websocket):
         await websocket.send(message)
 
 
+async def gestion(websocket):
+    connecte = False
+    async for message in websocket:
+        if not connecte:
+            connecte = connection(websocket, message)
+        else:
+            # on est connecté
+            await action(websocket, message)
+
+
+def connection(websocket, message):
+    print("Message reçu :", message)  # je fais des tests ici
+    print("user connecté")
+
+    # print(str(websocket.remote_address) + " s'est connecté")
+
+
+async def action(websocket, message):
+    print("action en cours")
+
+
 async def main():
-    async with serve(echo, "localhost", 8765):
-        await asyncio.Future()  # run forever
+    async with serve(gestion, host="localhost", port=8765):
+        await asyncio.Future()  # boucle infinie qui ne gène pas l'exécution du reste du programme
 
 
 asyncio.run(main())
