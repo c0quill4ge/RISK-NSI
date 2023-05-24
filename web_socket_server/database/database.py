959d9e50bf9fc4759447c32334a384fc3494f276
Nathan <118510193+Shay54FRA@users.noreply.github.com>
2023-05-24 09:43:22 +0200

    Update database.py
    new updateProperty function

--- web_socket_server/database/database.py
+++ web_socket_server/database/database.py
@@ -28,17 +28,22 @@ class Database:
     
     def updateArmy(self, idpartie, coord, nb_troupe):  # Met à jour le nombre de troupe sur une case
         idcase = getCase(idpartie, coord)[2]
-        query = "UPDATE etat partie SET nb_pions VALUES ? WHERE id_partie = ? AND id_case = ? ;"
+        query = "UPDATE etat_partie SET nb_pions VALUES ? WHERE id_partie = ? AND id_case = ? ;"
         self.cursor.execute(query, (nb_troupe, idpartie, idcase))
 
     def getPartie(self, idpartie):  # renvoie toutes les infos concernant les cases de la partie
-        query = "SELECT (*) FROM etat partie WHERE id_partie = ? ;"
+        query = "SELECT (*) FROM etat_partie WHERE id_partie = ? ;"
         self.cursor.execute(query, (idpartie,))
         return self.cursor.fetchall()  # Renvoie liste de tuples [(id_partie, id_case, id_joueur, nb_pions)], avec un tuple par case
 
     def getCase(self, idpartie, coord):  # coord est un tuple (x,y)
-        query = "SELECT nb_pions, id_joueurs, id_case FROM etat partie JOIN case ON etat_partie.id_case = case.id_case WHERE id_partie = ? AND x = ? AND y = ?"
+        query = "SELECT nb_pions, id_joueurs, id_case FROM etat_partie JOIN case ON etat_partie.id_case = case.id_case WHERE id_partie = ? AND x = ? AND y = ?"
         self.cursor.execute(query, (idpartie, coord[0], coord[1]))
         case = self.cursor.fetchall()  # Unr liste contenant un seul tuple
         case = case[0]  # Afin de récupérer le seul tuple de la liste
         return case  # Renvoie le tuple du nb de pions et l'id du joueur et celui de la case
+    
+    def updateProperty(self, idpartie, idcase, id_new_owner):  # Lorsqu'un joueur prend un pays
+        query = "UPDATE id_joueur FROM etat_partie VALUES ? WHERE id_case = ? AND id_partie = ?;"
+        self.cursor.execute(query, (id_new_owner, idcase, idpartie))
+        
