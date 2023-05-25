6f6195ea07aabe720583bbf42345ccca9e1a058a
vraiSlophil <slophil.contact@gmail.Com>
2023-05-25 10:04:58 +0200

    Changes by gabin

--- PYTHON/etat_plateau.py
+++ PYTHON/etat_plateau.py
@@ -50,8 +50,15 @@ def tour(id_partie):
     pass
 
 def donner_troupes(id_partie, joueur):
-	#en fonction du nombre de pays possédés, le jeu donne n troupes au joueur pendant la partie
-    pass
+    def donner_troupes(D,id_partie, joueur): # D => base de donnée sous forme de dictionnaire
+        nb_territoires = 0
+        for partie in D["etat_partie"]:
+            if partie["id_partie"] == id_partie and joueur == partie["id_joueur"]:
+                nb_territoires = len(partie["id_cases"])
+
+                nb_troupes_a_ajouter = nb_territoires // 3  # on donne autant d'armées que le joueur a de territoires divisé par 3 (sans le reste lol)
+                partie["nb_pions"] += nb_troupes_a_ajouter
+                break
 
 def debut_partie(id_partie):
 	#donne n troupes à tous les joueurs
