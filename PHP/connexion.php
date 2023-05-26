650e54891002fff29e0b1b4c14562cfaa9e42ca1
chatongarou <84314532+chatongarou@users.noreply.github.com>
2023-05-26 08:19:50 +0200

    Create connexion.php

---
+++ PHP/connexion.php
@@ -0,0 +1,31 @@
+<html>
+<head>
+  <title>Connexion Risk</title>
+  <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+  <div class="header">
+<section>
+
+</section>
+  </div>
+
+  <form method="post" class="content" action="analyse.php?sens=connexion">
+
+  	<div>
+  	  <label>Pseudonyme:</label>
+  	  <input type="text" name="pseudo">
+  	</div>
+  	<div >
+  	  <label>MDP:</label>
+  	  <input type="password" name="mdp">
+  	</div>
+  	<div>
+  	  <button type="submit">Se connecter</button>
+  	</div>
+  	<p>
+  		Vous ne possédez pas de compte ? <a href="incription.php">Connexion</a>
+  	</p>
+  </form>
+</body>
+</html>
