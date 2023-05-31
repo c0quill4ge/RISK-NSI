67ea6e51a7e39a4c06489713bceca5d08ccb9aa2
anthoshka <77466619+ahollard2@users.noreply.github.com>
2023-05-31 15:44:39 +0200

    Update class_graphe.py
    added class file for breadth search algorithm

--- PYTHON/class_graphe.py
+++ PYTHON/class_graphe.py
@@ -121,3 +121,36 @@ class Graphe:
         for i in range(len(chemin) - 1):
             p += self.__adj[chemin[i]][chemin[i+1]]
         return p
+class File:
+    def __init__(self):
+        self.__data = []
+        self.__tete = 0
+
+    def enfiler(self,e):
+        self.__data.append(e)
+
+    def defiler(self): # lance IndexError si la File est vide
+        if self.est_vide():
+            raise(IndexError("file vide"))
+        """
+        x = self.__data[0]
+        del self.__data[0]
+        return x
+        ### la complexité de cette fonction est mauvaise
+        """
+
+        if self.__tete > len(self):
+            self.__data = self.__data[self.__tete:]
+            self.__tete = 0
+            # psrint("ménage")
+        ## Maintenant, on ne procède au décalage qu'une fois de temps en temps
+        self.__tete += 1
+        return self.__data[self.__tete -1]
+
+        
+
+    def est_vide(self):
+        return len(self) == 0
+
+    def __len__(self):
+        return len(self.__data)-self.__tete
