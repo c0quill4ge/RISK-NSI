4549ff19fc4504bf3581bf95762f59a75687bbbe
GabinM <127486279+GabinM@users.noreply.github.com>
2023-06-02 10:10:44 +0200

    Update attaquer function from Martin in etat_plateau.py

--- PYTHON/etat_plateau.py
+++ PYTHON/etat_plateau.py
@@ -28,33 +28,35 @@ for a,b in graphlist:
 #graphe crée 
 
 db = Database
-def attaquer(database, graphe, idpartie, id_joueur, id_case_dep, id_case_cib, nb_troupe):  # Possible que si le nb de troupe est strictement supérieur à 1
+def attaquer(database, graphe, idpartie, id_attaquant, id_case_dep, id_case_cib,
+             nb_troupe, nb_troupes_envoyées):  # Possible que si le nb de troupe est strictement supérieur à 1
     nb_pions_case_dep, id_joueur = database.getCase(idpartie, id_case_dep)
     nb_pions_case_cib, id_joueur_ennemie = database.getCase(idpartie, id_case_cib)
     # On s'assure que le nb de troupes n'est pas abusé, que l'on attaque un autre joueur et que le chemin entre les 2 cases existent
-    if nb_troupe <= nb_pions_case_dep and id_joueur != id_joueur_ennemie and graphe.verifier_voisins(id_case_dep, id_case_cib): 
-    	pertes = bataille_des(nb_troupe, nb_pions_case_cib)
-    	new_nb_troupe_att = nb_troupe - pertes[0]
-    	new_nb_troupe_def = nb_troupe_def - pertes[1]
-    	if new_nb_troupe_att < 1:  # L'attaquant ne peux pas avoir moins d'une troupe sur son pays
+    if nb_troupe <= nb_pions_case_dep and id_joueur != id_joueur_ennemie and graphe.verifier_voisins(id_case_dep,
+                                                                                                     id_case_cib):
+        pertes = bataille_des(nb_troupe, nb_pions_case_cib)
+        new_nb_troupe_att = nb_troupe - pertes[0]
+        new_nb_troupe_def = nb_troupe_def - pertes[1]
+        if new_nb_troupe_att < 1:  # L'attaquant ne peux pas avoir moins d'une troupe sur son pays
             new_nb_troupe_att = 1
-    	if new_nb_troupe_def < 1:  # Le défenseur a perdu son pays
+        if new_nb_troupe_def < 1:  # Le défenseur a perdu son pays
             database.updateProperty(idpartie, id_case_cib, id_joueur)
-	
-	    #verifier si le joueur possede un continent en entier: envoyer le message qu'il a obtenu tout un continent donc qu'il possede un bonus
-	   
-	
+            change_couleur(idpartie, id_case_cib, id_attaquant)
+            deplacer_troupes(idpartie,id_case_dep,id_case_cib,nb_troupes_envoyées)
+            # verifier si le joueur possede un continent en entier: envoyer le message qu'il a obtenu tout un continent donc qu'il possede un bonus
+
             database.updateArmy(idpartie, id_case_cib, 1)
-    	else:
+        else:
             database.updateArmy(idpartie, id_case_cib, new_nb_troupe_def)
-    	database.updateArmy(idpartie, id_case_dep, new_nb_troupe_att)  # Met à jour l'armée de l'attaquant
-	
+        database.updateArmy(idpartie, id_case_dep, new_nb_troupe_att)  # Met à jour l'armée de l'attaquant
+
     elif nb_troupe > nb_pions_case_dep:
-	raise NbPionsInsuffisant
+        raise NbPionsInsuffisant
     elif id_joueur == id_joueur_ennemie:
-	raise CaseNonValide
+        raise CaseNonValide
     elif not graphe.verifier_voisins(id_case_dep, id_case_cib):
-	raise CasesNonConnectes
+        raise CasesNonConnectes
 
 def bataille_des(pions_att, pions_def):  # Renvoie le tuple des pertes de chaque côtés
     if pions_att >= 3:
