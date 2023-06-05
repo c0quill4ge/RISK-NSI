7442a31d909c24d4ec9c14d949ff54d1648f4a6a
JohnnnyMB <118377677+JohnnnyMB@users.noreply.github.com>
2023-06-05 11:47:59 +0200

    Update etat_plateau.py

--- PYTHON/etat_plateau.py
+++ PYTHON/etat_plateau.py
@@ -3,7 +3,7 @@ import random
 from web_socket_server/database/database.py import Database
 
 class NbPionsInsuffisant(Exception):
-    "lorsque le nombre de pions qu'on depalce ou avec lequel on attaque est trop grand"
+    "lorsque le nombreeeee de pions qu'on depalce ou avec lequel on attaque est trop grand"
     pass
 class CasesNonConnectes(Exception):
     "lorsque on ne peut pas passer d'une case a l'autre sans passer sur le terriotire d'autrui ou alors ces cases ne sont pas voisines"
